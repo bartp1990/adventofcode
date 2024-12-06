e035606f9e03672849286c278a16b8badb85ff37
Bart <bart@bart-peeters.nl>
2024-12-06 23:21:27 +0100

    Back to working part 1

--- 2024/6/main.py
+++ 2024/6/main.py
@@ -2,14 +2,10 @@
 
 import logging
 from enum import Enum
-from time import sleep
-import os
-
 import numpy as np
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-
 
 class Direction(Enum):
     UP = "^"
@@ -35,6 +31,7 @@ def get_next_direction(dir: Direction) -> Direction:
 class Grid:
     def __init__(self, f):
         self.grid = []
+        self.positions_visited = set()
         self.pos = None
         self.direction: Direction
         self.total_visited = 1
@@ -47,39 +44,11 @@ class Grid:
                 row.append(char)
                 if char in [direction.value for direction in Direction]:
                     self.pos = (i_row, i_col)
+                    self.starting_pos = self.pos
                     self.direction = char_to_direction[char]
 
             tmp_grid.append(row)
             self.grid = np.array(tmp_grid)
-
-    def check_loop_if_turn(self, new_pos):
-        rows, cols = self.grid.shape
-        cells = []
-        direction = get_next_direction(self.direction)
-
-        if direction == Direction.UP:
-            # Collect cells upwards, till we go out of bounds
-            for r in range(self.pos[0] - 1, -1, -1):  # Move from start_row upwards (r-- till 0)
-                cells.append((r, self.pos[1]))
-        elif direction == Direction.DOWN:
-            # Collect cells downwards, till we go out of bounds
-            for r in range(self.pos[0] - 1, rows):  # Move from start_row downwards (r++ till rows)
-                cells.append((r, self.pos[1]))
-        elif direction == Direction.LEFT:
-            # Collect cells leftwards, till we go out of bounds
-            for c in range(self.pos[1] - 1, -1, -1):  # Move from start_col leftwards (c-- till 0)
-                cells.append((self.pos[0], c))
-        elif direction == Direction.RIGHT:
-            # Collect cells rightwards, till we go out of bounds
-            for c in range(self.pos[1] - 1, cols):  # Move from start_col rightwards (c++ till cols)
-                cells.append((self.pos[0], c))
-
-        for i, cell in enumerate(cells):
-            print(self.grid[*cell])
-            if self.grid[*cell] in [direction.value for direction in Direction] and self.grid[*cells[i+1]] == "#":
-                self.possible_obstacles.append(new_pos)
-            if self.grid[*cell] == "#":
-                break
 
     def move(self):
 
@@ -110,10 +79,6 @@ class Grid:
         if new_pos and self.grid[*new_pos] != "#":
             if self.grid[*new_pos] not in [dir.value for dir in Direction]:
                 self.total_visited += 1
-            else:
-                bla = 1
-
-            self.check_loop_if_turn(new_pos=new_pos)
 
             self.pos = new_pos
         else:
@@ -129,3 +94,5 @@ if __name__ == "__main__":
 
     while True:
         grid.move()
+
+
