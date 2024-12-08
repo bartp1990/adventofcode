513d3f47f357c4e1bbdd6a55fbf852c0ac762c82
Bart <bart@bart-peeters.nl>
2024-12-08 16:08:52 +0100

    Working part 1, but other technique

--- 2024/8/main.py
+++ 2024/8/main.py
@@ -7,13 +7,40 @@ import numpy as np
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
+def print_grid(anti_nodes):
+    grid = np.full((max_y+1, max_x+1), ".")
+    for coord in anti_nodes:
+        grid[*coord] = "#"
+
+    for row in grid:
+        print(''.join(map(str, row)))
+
+    print("\n")
+
+def get_anti_nodes(include_antennas):
+    anti_nodes = set()
+    for _, coords in pos_per_type.items():
+        permutations = set(itertools.permutations(coords, 2))
+
+        for p in permutations:
+            for i in range(1, 2):
+                new_pos_y = p[0][0] - (p[0][0] - p[1][0]) * i * 2
+                new_pos_x = p[0][1] - (p[0][1] - p[1][1]) * i * 2
+
+                if max_y < new_pos_y or new_pos_y < 0 or max_x < new_pos_x or new_pos_x < 0:
+                    continue
+
+                anti_nodes.add((new_pos_y, new_pos_x))
+
+
+    print_grid(anti_nodes)
+    return len(anti_nodes)
+
 if __name__ == "__main__":
     with open("input.txt") as f:
         lines = f.readlines()
 
     pos_per_type = dict()
-    anti_nodes = set()
-
     max_y = len(lines) - 1
     max_x = len(list(lines[0].strip())) - 1
 
@@ -26,36 +53,8 @@ if __name__ == "__main__":
                 coord = (y, x)
                 pos_per_type[char].add(coord)
 
-    for _, coords in pos_per_type.items():
-        permutations = set(itertools.permutations(coords, 2))
-        for per in permutations:
-
-            for i in range(1, 2):
-                diff_y = per[0][0] - per[1][0] * i
-                diff_x = per[0][1] - per[1][1] * i
-
-                new_pos_y = per[1][0] - diff_y
-                new_pos_x = per[1][1] - diff_x
-
-                if max_y < new_pos_y or new_pos_y < 0 or max_x < new_pos_x or new_pos_x < 0:
-                    continue
-
-                new_pos = (new_pos_y, new_pos_x)
-
-                if new_pos not in anti_nodes:
-                    anti_nodes.add(new_pos)
-
-    # grid = np.full((max_y+1, max_x+1), ".")
-    # for t, coords in pos_per_type.items():
-    #     for coord in coords:
-    #         grid[*coord] = t
-    #
-    # for row in grid:
-    #     print(''.join(map(str, row)))
-
-    part_1 = len(anti_nodes)
-    part_2 = None
+    part_1 = get_anti_nodes(include_antennas=False)
 
     logger.info("Advent of Code 2024 | Day 8")
     logger.info(f"Answer part 1: {part_1}")
-    logger.info(f"Answer part 2: {part_2}")
+    # logger.info(f"Answer part 2: {part_2}")
