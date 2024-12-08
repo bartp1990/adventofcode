cb0316456134c4c2f4bd05908fdcef1dcbbb8dff
Bart <bart@bart-peeters.nl>
2024-12-08 16:51:44 +0100

    Working part 2 but not part 1

--- 2024/8/main.py
+++ 2024/8/main.py
@@ -17,21 +17,25 @@ def print_grid(anti_nodes):
 
     print("\n")
 
-def get_anti_nodes(include_antennas):
+def get_anti_nodes_part2():
     anti_nodes = set()
     for _, coords in pos_per_type.items():
-        permutations = set(itertools.permutations(coords, 2))
+        combinations = set(itertools.combinations(coords, 2))
 
-        for p in permutations:
-            for i in range(1, 2):
-                new_pos_y = p[0][0] - (p[0][0] - p[1][0]) * i * 2
-                new_pos_x = p[0][1] - (p[0][1] - p[1][1]) * i * 2
+        for c in combinations:
 
-                if max_y < new_pos_y or new_pos_y < 0 or max_x < new_pos_x or new_pos_x < 0:
+            first = c[0]
+            dy = (c[0][0] - c[1][0])
+            dx = (c[0][1] - c[1][1])
+
+            for i in range(-max_x, max_x):
+                y = first[0] - dy * i
+                x = first[1] - dx * i
+
+                if max_y < y or y < 0 or max_x < x or x < 0:
                     continue
 
-                anti_nodes.add((new_pos_y, new_pos_x))
-
+                anti_nodes.add((y, x))
 
     print_grid(anti_nodes)
     return len(anti_nodes)
@@ -53,8 +57,8 @@ if __name__ == "__main__":
                 coord = (y, x)
                 pos_per_type[char].add(coord)
 
-    part_1 = get_anti_nodes(include_antennas=False)
+    part_2 = get_anti_nodes_part2()
 
     logger.info("Advent of Code 2024 | Day 8")
-    logger.info(f"Answer part 1: {part_1}")
-    # logger.info(f"Answer part 2: {part_2}")
+    # logger.info(f"Answer part 1: {part_1}")
+    logger.info(f"Answer part 2: {part_2}")
