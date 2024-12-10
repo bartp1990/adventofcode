1dfa7f18852f0253a25cabc6387b308faa2408ce
Bart <bart@bart-peeters.nl>
2024-12-10 01:40:40 +0100

    Day 9 part 2, slow

--- 2024/9/main.py
+++ 2024/9/main.py
@@ -1,11 +1,10 @@
 """Solution for https://adventofcode.com/2024/day/9."""
 
 import logging
-from dataclasses import dataclass
-from typing import Self
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+
 
 def get_checksum(blocks: list) -> int:
     checksum = 0
@@ -15,16 +14,25 @@ def get_checksum(blocks: list) -> int:
 
     return checksum
 
-def get_blocks(line: list) -> list:
+
+def get_blocks(line: list):
     blocks = []
+    file_offset = 0
+    file_table = dict()
     counter = 0
     for i in range(0, len(line), 2):
-        blocks.extend([counter] * line[i])
+        file_content = [counter] * line[i]
+        blocks.extend(file_content)
+        file_table[counter] = (counter, file_offset, len(file_content))
+        file_offset += len(file_content)
         if i + 1 < len(line):
-            blocks.extend(["."] * line[i + 1])
+            free_space = ["."] * line[i + 1]
+            blocks.extend(free_space)
+            file_offset += len(free_space)
         counter += 1
 
-    return blocks
+    return blocks, file_table
+
 
 def format_part_1(blocks: list) -> list:
     blocks = blocks.copy()
@@ -46,19 +54,53 @@ def format_part_1(blocks: list) -> list:
 
     return blocks
 
-def format_part_2(blocks: list) -> list:
-    pass
+
+def find_consecutive_free_space(blocks):
+    indices = []
+    start = None
+
+    for i in range(len(blocks)):
+        if blocks[i] == ".":
+            if start is None:
+                start = i
+        else:
+            if start is not None:
+                indices.append((start, i))
+                start = None
+
+    if start is not None:
+        indices.append((start, len(blocks)))
+
+    return indices
+
+
+def format_part_2(blocks: list, file_table: dict) -> list:
+    blocks = blocks.copy()
+    files = list(value for value in file_table.values())
+
+    for num, offset, size in sorted(files, key=lambda x: x[0], reverse=True):
+        for space in find_consecutive_free_space(blocks):
+            diff = space[1] - space[0]
+            if diff >= size:
+                if offset < space[0]:
+                    break
+
+                blocks[space[0] : space[0] + size] = blocks[offset : offset + size]
+                blocks[offset : offset + size] = ["."] * size
+
+    return blocks
+
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         line = f.read()
 
-    disk = list(map(int,list(line.strip())))
-    blocks = get_blocks(disk)
+    disk = list(map(int, list(line.strip())))
+    blocks, file_table = get_blocks(disk)
 
     part_1 = get_checksum(format_part_1(blocks))
-    # part_2 = get_checksum(format_part_2(chunks))
+    part_2 = get_checksum(format_part_2(blocks, file_table))
 
     logger.info("Advent of Code 2024 | Day 9")
     logger.info(f"Answer part 1: {part_1}")
-    # logger.info(f"Answer part 2: {part_2}")
+    logger.info(f"Answer part 2: {part_2}")
