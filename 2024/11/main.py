bcedf52ade32a8949d441e914222e9a8259f827b
Bart <bart@bart-peeters.nl>
2024-12-12 08:17:45 +0100

    Day 11 part 2

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -6,10 +6,10 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 import math
+from functools import cache
+NUM_ITERATIONS = 75
 
-blink_cache = dict()
-leaf_nodes = []
-
+@cache
 def split_number(n, num_digits):
     n = abs(n)  # Ensure the number is positive
     split_point = num_digits // 2
@@ -20,47 +20,31 @@ def split_number(n, num_digits):
 
 def is_even_number_of_digits(n, num_digits):
     if n == 0:
-        return True  # 0 has 1 digit, so it's odd, but we can treat it as even if preferred
+        return True
     return num_digits % 2 == 0
 
-def blink(n: int, cur_it, num_iterations, total):
-    if n in blink_cache:
-        result = blink_cache[n]
+@cache
+def blink(n: int, cur_it):
+
+    if cur_it == NUM_ITERATIONS:
+        return 1
+
+    num_digits = math.floor(math.log10(n)) + 1 if n > 0 else 0
+    if n == 0:
+        return blink(1, cur_it+1)
+    elif is_even_number_of_digits(n, num_digits):
+        first, second = split_number(n, num_digits)
+        return blink(first, cur_it+1) + blink(second, cur_it+1)
     else:
-        num_digits = math.floor(math.log10(n)) + 1 if n > 0 else 0
-        if n == 0:
-            result = 1
-        elif is_even_number_of_digits(n, num_digits):
-            result = split_number(n, num_digits)
-        else:
-            result = n * 2024
-
-        if cur_it < num_iterations:
-            if isinstance(result, tuple):
-                for r in result:
-                    result, total = blink(r, cur_it + 1, num_iterations, total)
-            else:
-                result, total = blink(result, cur_it + 1, num_iterations, total)
-
-        blink_cache[n] = result
-
-    # Only count leaf nodes here (final step in the recursion)
-    leaf_nodes = globals()["leaf_nodes"]
-    leaf_nodes.append(result)
-
-    return result, total
+        return blink(n * 2024, cur_it+1)
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         stones = list(map(int, f.read().split()))
 
-        part_1 = 0
+        part_2 = 0
         for stone in stones:
-            _, total = blink(stone, 1, 75, 0)
-            part_1 += total
-
-
+            part_2 += blink(stone, 0)
 
     logger.info("Advent of Code 2024 | Day 11")
-    logger.info(f"Answer part 1: {len(leaf_nodes)}")
-    # logger.info(f"Answer part 2: {part_2}")
+    logger.info(f"Answer part 2: {part_2}")
