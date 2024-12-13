bf73895236d8ab7ee3fa5121972801265b470801
Bart <bart@bart-peeters.nl>
2024-12-13 16:06:03 +0100

    Small refactor

--- 2024/12/main.py
+++ 2024/12/main.py
@@ -51,15 +51,76 @@ class Region:
             elif (min_x_next == min_x_current or max_x_next == max_x_current) and len_row_next != len_row_current:
                 n_edges += 2
 
+        subgrid = self.get_subgrid_of_region(grid)
+        print_grid(subgrid)
+
         return n_edges
 
-    # subgrid = self.get_subgrid_of_region(grid)
+def find_regions_in_grid(grid):
+    shape = grid.shape
+    visited = set()
+    regions = []
+    for y in range(0, shape[0]):
+        for x in range(0, shape[1]):
+            coord = (x, y)
+            if coord not in visited:
+                letter = grid[*coord]
+                region = find_region(coord, letter, visited)
+                regions.append(region)
+    return regions
 
 
 def print_grid(grid: np.ndarray):
     for row in grid:
         print("".join(row))
     print("\n")
+
+
+def find_region(coord, char, visited):
+    region = Region(letter=char)
+
+    stack = [coord]
+
+    while stack:
+        current_coord = stack.pop()
+        if current_coord in visited:
+            continue
+        visited.add(current_coord)
+
+        region.area += 1
+        if current_coord[0] not in region.rows:
+            region.rows[current_coord[0]] = []
+        region.rows[current_coord[0]].append(current_coord[1])
+
+        if current_coord[0] in (0, shape[0]):
+            region.outside_edges_touched += 1
+        if current_coord[1] in (0, shape[1]):
+            region.outside_edges_touched += 1
+
+        neighbours = [
+            (current_coord[0] - 1, current_coord[1]),
+            (current_coord[0] + 1, current_coord[1]),
+            (current_coord[0], current_coord[1] - 1),
+            (current_coord[0], current_coord[1] + 1),
+        ]
+
+        for n_coord in neighbours:
+            if 0 <= n_coord[0] < shape[0] and 0 <= n_coord[1] < shape[1]:
+                if grid[n_coord[0], n_coord[1]] == char and n_coord not in visited:
+                    stack.append(n_coord)
+                elif grid[n_coord[0], n_coord[1]] != char:
+                    region.perimeter += 1
+            else:
+                region.perimeter += 1
+
+            region.min_y = min(region.rows.keys())
+            region.max_y = max(region.rows.keys())
+            region.min_x = min(min(lst) for lst in region.rows.values())
+            region.max_x = max(max(lst) for lst in region.rows.values())
+
+    region.edges = region.find_edges()
+
+    return region
 
 
 if __name__ == "__main__":
@@ -77,64 +138,9 @@ if __name__ == "__main__":
     grid = np.array(tmp_grid)
     shape = grid.shape
 
-    def find_region(coord, char):
-        region = Region(letter=char)
-
-        stack = [coord]
-
-        while stack:
-            current_coord = stack.pop()
-            if current_coord in visited:
-                continue
-            visited.add(current_coord)
-
-            region.area += 1
-            if current_coord[0] not in region.rows:
-                region.rows[current_coord[0]] = []
-            region.rows[current_coord[0]].append(current_coord[1])
-
-            if current_coord[0] in (0, shape[0]):
-                region.outside_edges_touched += 1
-            if current_coord[1] in (0, shape[1]):
-                region.outside_edges_touched += 1
-
-            neighbours = [
-                (current_coord[0] - 1, current_coord[1]),
-                (current_coord[0] + 1, current_coord[1]),
-                (current_coord[0], current_coord[1] - 1),
-                (current_coord[0], current_coord[1] + 1),
-            ]
-
-            for n_coord in neighbours:
-                if 0 <= n_coord[0] < shape[0] and 0 <= n_coord[1] < shape[1]:
-                    if grid[n_coord[0], n_coord[1]] == char and n_coord not in visited:
-                        stack.append(n_coord)
-                    elif grid[n_coord[0], n_coord[1]] != char:
-                        region.perimeter += 1
-                else:
-                    region.perimeter += 1
-
-                region.min_y = min(region.rows.keys())
-                region.max_y = max(region.rows.keys())
-                region.min_x = min(min(lst) for lst in region.rows.values())
-                region.max_x = max(max(lst) for lst in region.rows.values())
-
-        region.edges = region.find_edges()
-
-        return region
-
-    visited = set()
-    regions = []
-    for y in range(0, shape[0]):
-        for x in range(0, shape[1]):
-            coord = (x, y)
-            if coord not in visited:
-                letter = grid[*coord]
-                region = find_region(coord, letter)
-                regions.append(region)
-
     part_1 = 0
     part_2 = 0
+    regions = find_regions_in_grid(grid)
     for region in regions:
         logger.info(region)
         part_1 += region.area * region.perimeter
