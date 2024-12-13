fe82159fc2ba45d9cdbd99a55dcb737861d6deab
Bart <bart@bart-peeters.nl>
2024-12-13 13:52:30 +0100

    Cleanup

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -7,36 +7,40 @@ logger = logging.getLogger()
 
 import math
 from functools import cache
+
 NUM_ITERATIONS = 75
+
 
 @cache
 def split_number(n, num_digits):
     n = abs(n)  # Ensure the number is positive
     split_point = num_digits // 2
-    divisor = 10 ** split_point
+    divisor = 10**split_point
     first = n // divisor
     second = n % divisor
     return first, second
+
 
 def is_even_number_of_digits(n, num_digits):
     if n == 0:
         return True
     return num_digits % 2 == 0
 
+
 @cache
 def blink(n: int, cur_it):
-
     if cur_it == NUM_ITERATIONS:
         return 1
 
     num_digits = math.floor(math.log10(n)) + 1 if n > 0 else 0
     if n == 0:
-        return blink(1, cur_it+1)
+        return blink(1, cur_it + 1)
     elif is_even_number_of_digits(n, num_digits):
         first, second = split_number(n, num_digits)
-        return blink(first, cur_it+1) + blink(second, cur_it+1)
+        return blink(first, cur_it + 1) + blink(second, cur_it + 1)
     else:
-        return blink(n * 2024, cur_it+1)
+        return blink(n * 2024, cur_it + 1)
+
 
 if __name__ == "__main__":
     with open("input.txt") as f:
