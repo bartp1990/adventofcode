e5dca83aac63bd839228a3b18c29ed1f8a6e792c
Bart <bart@bart-peeters.nl>
2024-12-13 13:53:56 +0100

    Day 12 part 1 + bit of 2

--- 2024/12/main.py
+++ 2024/12/main.py
@@ -1,8 +1,37 @@
+"""Solution for https://adventofcode.com/2024/day/12."""
+
 import logging
 import numpy as np
 
+from dataclasses import dataclass, field
+from enum import Enum, auto
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+
+class OutsideEdge(Enum):
+    NORTH = auto()
+    SOUTH = auto()
+    EAST = auto()
+    WEST = auto()
+
+@dataclass
+class Region:
+    letter:str
+    rows: dict = field(default_factory=dict)
+    area:int = 0
+    perimeter:int = 0
+    min_x:int = 999999999999
+    max_x:int = 0
+    min_y:int = 999999999999
+    max_y:int = 0
+    outside_edges_touched: set[OutsideEdge] = field(default_factory=set)
+    edges:int = 0
+
+def print_grid(grid: np.ndarray):
+    for row in grid:
+        print("".join(row))
+    print("\n")
 
 if __name__ == "__main__":
     with open("input.txt") as f:
@@ -19,11 +48,12 @@ if __name__ == "__main__":
     grid = np.array(tmp_grid)
     shape = grid.shape
 
-    visited = set()
+    def find_edges(region):
+        pass
+
 
     def find_region(coord, char):
-        area = 0
-        perimeter = 0
+        region = Region(letter=char)
 
         stack = [coord]
 
@@ -32,12 +62,16 @@ if __name__ == "__main__":
             if current_coord in visited:
                 continue
             visited.add(current_coord)
-            area += 1
+
+            region.area += 1
+            if current_coord[0] not in region.rows:
+                region.rows[current_coord[0]] = []
+            region.rows[current_coord[0]].append(current_coord[1])
             neighbours = [
-                (current_coord[0] - 1, current_coord[1]),  # Up
-                (current_coord[0] + 1, current_coord[1]),  # Down
-                (current_coord[0], current_coord[1] - 1),  # Left
-                (current_coord[0], current_coord[1] + 1),  # Right
+                (current_coord[0] - 1, current_coord[1]),
+                (current_coord[0] + 1, current_coord[1]),
+                (current_coord[0], current_coord[1] - 1),
+                (current_coord[0], current_coord[1] + 1),
             ]
 
             for n_coord in neighbours:
@@ -45,22 +79,34 @@ if __name__ == "__main__":
                     if grid[n_coord[0], n_coord[1]] == char and n_coord not in visited:
                         stack.append(n_coord)
                     elif grid[n_coord[0], n_coord[1]] != char:
-                        perimeter += 1
+                        region.perimeter += 1
                 else:
-                    perimeter += 1
-        return area, perimeter
+                    region.perimeter += 1
 
+                region.min_y = min(region.rows.keys())
+                region.max_y = max(region.rows.keys())
+                region.min_x = min(min(lst) for lst in region.rows.values())
+                region.max_x = max(max(lst) for lst in region.rows.values())
+
+        return region
+
+
+    visited = set()
     regions = []
-
     for y in range(0, shape[0]):
         for x in range(0, shape[1]):
             coord = (x, y)
             if coord not in visited:
-                char = grid[coord[0], coord[1]]
-                area, perimeter = find_region(coord, char)
-                regions.append({"char": char, "area": area, "perimeter": perimeter})
+                letter = grid[*coord]
+                region = find_region(coord, letter)
+                regions.append(region)
 
+    part_1 = 0
+    part_2 = 0
     for region in regions:
-        logger.info(f"Region: Char = {region['char']}, Area = {region['area']}, Perimeter = {region['perimeter']}")
+        part_1 += region.area * region.perimeter
+        part_2 += region.area * region.edges
 
-    print(sum(region['area'] * region['perimeter'] for region in regions))
+    logger.info("Advent of Code 2024 | Day 12")
+    logger.info(f"Answer part 1: {part_1}")
+    logger.info(f"Answer part 1: {part_2}")
